c801d5cad726a2d8d7ddf53d03d5dbaaf64e46c6
Alex Kaszynski <akascap@gmail.com>
2021-01-11 11:51:47 -0700

    initial move from pymapdl

---
+++ ansys/mapdl/reader/_version.py
@@ -0,0 +1,7 @@
+"""Version of ansys-mapdl-core module."""
+
+# major, minor, patch
+version_info = 0, 44, 21
+
+# Nice string for the version
+__version__ = '.'.join(map(str, version_info))
---
+++ ansys/mapdl/reader/cython/parsefull.h
@@ -0,0 +1,4 @@
+int return_fheader(char*, int*);
+
+void read_full(int*, int*, int*, int*, int*, double*, int*, int*, double*, 
+               int*, char*, int*, int);
