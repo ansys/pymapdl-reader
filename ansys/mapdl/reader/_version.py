53b000ebc36dc0d7e628cd151c283da5cc1f1e6b
Alex Kaszynski <akascap@gmail.com>
2021-02-12 08:30:16 -0700

    build on numpy<1.20.0 due to binary compatability (#12)

--- ansys/mapdl/reader/_version.py
+++ ansys/mapdl/reader/_version.py
@@ -1,7 +1,7 @@
 """Version of ansys-mapdl-reader module."""
 
 # major, minor, patch
-version_info = 0, 50, 4
+version_info = 0, 50, 5
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
