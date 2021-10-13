62edb6a7781cb8dc757bf216b6d6082785342dbb
Beppo <53090100+beppo-dd@users.noreply.github.com>
2021-10-13 13:48:12 +0200

    cython._binary_reader.cells_with_*_nodes: Generalize and simplify by directly using `offset` for next cell offset
    - Therefore the function `cell_lookup`, which was limited to 3D cells, is no longer necessary.
    - Now, all cell types are supported, while the functions has even been simplified.
    - Now, the functions do not need the parameter `celltypes` any more. Since they are only used in rst.py in the private method `Result._extract_node_components`, I took the liberty to remove the parameter A in the respective definition und the respective call.
    
    Remark: Only tested with a pure python version of the functions, please test the cython function of the commit!

--- ansys/mapdl/reader/rst.py
+++ ansys/mapdl/reader/rst.py
@@ -723,10 +723,10 @@ class Result(AnsysBinary):
         # need to extract the mesh
         cells, offset = vtk_cell_info(grid)
         if sel_type_all:
-            cell_mask = cells_with_all_nodes(offset, cells, grid.celltypes,
+            cell_mask = cells_with_all_nodes(offset, cells,
                                              mask.view(np.uint8))
         else:
-            cell_mask = cells_with_any_nodes(offset, cells, grid.celltypes,
+            cell_mask = cells_with_any_nodes(offset, cells,
                                              mask.view(np.uint8))
 
         if not cell_mask.any():
