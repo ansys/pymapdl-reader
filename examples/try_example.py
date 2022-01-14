b436c5126b06ba4b6c5e21309bbbe3eaabbce097
Roman Ilchenko <romailch1308@gmail.com>
2022-01-14 21:26:43 +0100

    new test

--- examples/try_example.py
+++
@@ -1,8 +0,0 @@
-import pyvista
-import numpy as np
-
-from ansys.mapdl import reader as pymapdl_reader
-from ansys.mapdl.reader import examples
-
-# Download an example shaft modal analysis result file
-shaft = examples.download_shaft_modal()
