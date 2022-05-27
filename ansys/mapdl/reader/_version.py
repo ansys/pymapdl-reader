502303f42f0d9f31c8ce19b1c9ec69321d7a7fea
Alex Kaszynski <akascap@gmail.com>
2022-05-27 14:29:40 -0600

    bump to v0.51.14

--- ansys/mapdl/reader/_version.py
+++ ansys/mapdl/reader/_version.py
@@ -8,7 +8,7 @@ version_info = 0, 58, 'dev0'
 """
 
 # major, minor, patch
-version_info = 0, 51, 13
+version_info = 0, 51, 14
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
