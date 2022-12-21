b43e0da7e8e4332aaa4935d124409ebc70ba8e13
German <germanmartinezayuso@gmail.com>
2022-12-21 10:39:38 +0100

    Bumping to 0.52.5

--- ansys/mapdl/reader/_version.py
+++ ansys/mapdl/reader/_version.py
@@ -8,7 +8,7 @@ version_info = 0, 58, 'dev0'
 """
 
 # major, minor, patch
-version_info = 0, 52, 4
+version_info = 0, 52, 5
 
 # Nice string for the version
 __version__ = ".".join(map(str, version_info))
