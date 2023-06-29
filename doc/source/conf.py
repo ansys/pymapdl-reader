5e32b843cf89a953734e4d4ca0c27c62e7c6a6f5
Camille <78221213+clatapie@users.noreply.github.com>
2023-06-29 16:34:15 +0200

    fix: pre-commit fixes

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -5,6 +5,7 @@ import warnings
 from ansys_sphinx_theme import get_version_match, pyansys_logo_black
 import pyvista
 from sphinx_gallery.sorting import FileNameSortKey
+
 from ansys.mapdl.reader import __version__
 
 cname = os.getenv("DOCUMENTATION_CNAME", "<DEFAULT_CNAME>")
